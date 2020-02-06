b04cb4113a948ccd077f81f2c79a6c7a83e90421
Section2 <Section2@DESKTOP-KHOPMPR>
2020-02-06 21:27:39 +0300

    Commit 4

--- 036_MoviesMvcWissen/Controllers/MoviesController.cs
+++ 036_MoviesMvcWissen/Controllers/MoviesController.cs
@@ -1,9 +1,12 @@
 ﻿using _036_MoviesMvcWissen.Contexts;
 using _036_MoviesMvcWissen.Entities;
+using _036_MoviesMvcWissen.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Globalization;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,14 +18,46 @@ namespace _036_MoviesMvcWissen.Controllers
         // GET: Movies
         public ViewResult Index()
         {
-            var model = db.Movies.ToList();
+            //var model = db.Movies.ToList();
+            var model = GetList();
+            //ViewBag.count = model.Count;
+            ViewData["count"] = model.Count;
             return View(model);
+        }
+
+        [NonAction]//url üzerinden çağıralamaz hale getirmek için.
+        public List<Movie> GetList(bool removeSession = true)
+        {
+            List<Movie> entities;
+            if (removeSession)
+            {
+                Session.Remove("movies");
+            }
+            if (Session["movies"] == null || removeSession)
+            {
+                entities = db.Movies.ToList();
+                Session["movies"] = entities;
+            }
+            else
+            {
+                entities = Session["movies"] as List<Movie>;
+            }
+            return entities;
+        }
+        
+        public ActionResult GetMoviesFromSession()
+        {
+            var model = GetList(false);
+            ViewBag.count = model.Count;
+            return View("Index", model);
         }
 
         [HttpGet]
         public ActionResult Add()
         {
-            return View();
+            ViewBag.Message = "Please enter movie information...";
+            //return View();
+            return new ViewResult();
         }
 
         [HttpPost]
@@ -46,8 +81,61 @@ namespace _036_MoviesMvcWissen.Controllers
             }
             db.Movies.Add(entity);
             db.SaveChanges();
+            TempData["Info"] = "Record successfully saved to database.";
             return RedirectToAction("Index"); 
 
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var model = db.Movies.Find(id.Value);
+            List<SelectListItem> years = new List<SelectListItem>();
+            SelectListItem year;
+            for (int i = DateTime.Now.Year; i >= 2000 ; i--)
+            {
+                year = new SelectListItem() { Value = i.ToString(), Text = i.ToString() };
+                years.Add(year);
+            }
+            ViewBag.Years = new SelectList(years, "Value", "Text", model.ProductionYear);
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "Id,Name,ProductionYear")]Movie movie, string BoxOfficeReturn)
+        {
+            var entity = db.Movies.SingleOrDefault(e => e.Id == movie.Id);
+            entity.Name = movie.Name;
+            entity.ProductionYear = movie.ProductionYear;
+            entity.BoxOfficeReturn = Convert.ToDouble(BoxOfficeReturn.Replace(",", "."), CultureInfo.InvariantCulture);
+            db.Entry(entity).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToRoute(new { controller = "Movies", action = "Index" });
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(400,"Id is required!");
+            }
+            var model = db.Movies.FirstOrDefault(e => e.Id == id.Value);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var entity = db.Movies.Find(id);
+            db.Movies.Remove(entity);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ 036_MoviesMvcWissen/Models/DropDownListModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _036_MoviesMvcWissen.Models
+{
+    public class DropDownListModel
+    {
+        public string Value { get; set; }
+        public string Text { get; set; }
+        public DropDownListModel()
+        {
+
+        }
+        public DropDownListModel(string value, string text)
+        {
+           
+            Value = value;
+            Text = text;
+        }
+    }
+}
